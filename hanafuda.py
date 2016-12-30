f7fef64bc6693534314ba83cf55ad180e5aa8734
star318.ss <star318.ss@gmail.com>
2016-12-31 01:28:13 +0900

    2016/12/30 complete

--- hanafuda.py
+++ hanafuda.py
@@ -20,18 +20,22 @@ idata_np=idata_np.reshape(data_length/2,2)
 print idata_np
 
 #idata[0]=sors of n,[1]=cut time
-#hanahuda time
+#hanahuda time:ht
+ht=0
+while 1:
 
-hanafuda=np.arange(1,idata_np[0,0]+1)
-hanafuda=hanafuda[::-1] #reverse
-cut_time=idata_np[0,1]
-for ct in range(1,cut_time+1):
+    hanafuda=np.arange(1,idata_np[ht,0]+1)
+    hanafuda=hanafuda[::-1] #reverse
+    cut_time=idata_np[ht,1]
+    for ct in range(1+ht,cut_time+1+ht):
+        p=idata_np[ct,0]
+        c=idata_np[ct,1]# ct + t  t:hanafudauda play time
+        c_work_space=hanafuda[p-1:p-1+c]
+        front_fuda=hanafuda[0:p-1]
+        back_fuda=hanafuda[p+c-1:]
+        hanafuda=np.hstack([c_work_space,front_fuda,back_fuda])
+
     print hanafuda
-    p=idata_np[ct,0]
-    c=idata_np[ct,1]# ct + t  t:hanafudauda play time
-    c_work_space=hanafuda[p-1:p-1+c]
-    front_fuda=hanafuda[0:p]
-    back_fuda=hanafuda[p+c:]
-    hanafuda=np.hstack([c_work_space,front_fuda,back_fuda])
-    print hanafuda
-    print c_work_space,front_fuda,back_fuda
+    ht=ht+cut_time+1
+    if idata_np[ht,0]==0 and idata_np[ht,1]==0:
+        break
